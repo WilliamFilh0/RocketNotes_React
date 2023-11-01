cfdf7bdddd953e5f7c8a8920bcfcb93727b9092b
WilliamFilh0 <williamfilho8908@gmail.com>
2023-11-01 11:50:44 -0300

    Imagem do Usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,21 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles';
 
 export function Header() {
   return (
     <Container>
-      
-     
+      <Profile>
+        <img
+          src="https://github.com/WilliamFilh0.png"
+          alt="Foto do Usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>William Filho</strong>
+        </div>
+
+      </Profile>
+
     </Container>
   )
-}
+} 

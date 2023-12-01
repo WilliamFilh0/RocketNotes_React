a0128b359b286e3d952d9a324630104097b048a6
WilliamFilh0 <williamfilho8908@gmail.com>
2023-12-01 16:18:26 -0300

    Local Storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from '../services/api'
 
@@ -10,11 +10,14 @@ function AuthProvider({ children }) {
 
   async function signIn({ email, password }) {
 
-    //Faz uma requisição de login para o servidor 
+
     try {
       const response = await api.post("/sessions", { email, password });
 
-      const { user, token } = response.data
+      const { user, token } = response.data;
+
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token)
 
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token })
@@ -27,6 +30,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, []);
 
 
 

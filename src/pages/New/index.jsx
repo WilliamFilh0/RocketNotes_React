afa1e21f25e2e31b126e29007f9833ee86b0d9ea
WilliamFilh0 <williamfilho8908@gmail.com>
2023-12-16 15:09:39 -0300

    Remover Tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New() {
   const [links, setLinks,] = useState([]);
   const [newLink, setNewLink] = useState("")
 
+  const [tags, setTags,] = useState([]);
+  const [newTag, setNewTag] = useState("")
+
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink])
     setNewLink("");
@@ -23,6 +26,14 @@ export function New() {
     setLinks(prevState => prevState.filter(link => link !== deleted))
   }
 
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag])
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  }
 
   return (
     <Container>
@@ -60,8 +71,23 @@ export function New() {
 
           <Section title="Marcadores">
             <div className='tags'>
-              <NoteItem value="react" />
-              <NoteItem isNew placeholder="Nova tag" />
+              {
+                tags.map((tag, index) => (
+                  <NoteItem
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />
+                ))
+              }
+
+              <NoteItem
+                isNew
+                placeholder="Nova tag"
+                onChange={e => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
